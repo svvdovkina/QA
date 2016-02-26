e45cb88076043704619adb522cb636d46b60ca9b
Svetlana Vdovkina <svvdovkina@gmail.com>
2016-02-27 00:14:38 +0300

    Add errors on y-axis

--- prog/GetQAsincos.cpp
+++ prog/GetQAsincos.cpp
@@ -1,6 +1,6 @@
 /**
 * Takes text files MeanCosSinNphi.txt from each run folder and makes graph <cos(nphi)>, <sin(nphi)> vs runnumber.
-* Puts graphs for different centralities to single .root file - QAt0offset.root
+* Puts graphs for different centralities to single .root file - "MeanSinCos.root.root
 *
 * S. Vdovkina svvdovkina@gmail.com
 **/
@@ -26,7 +26,9 @@ void GetQAsincos()
 
 	// **** reading mean cos and sin only for RXN18 det (S+N, out+in)
 	Float_t cos[2][NMUL][400]; // first dimension - nharm
+	Float_t ercos[2][NMUL][400]; // first dimension - nharm
 	Float_t sin[2][NMUL][400];
+	Float_t ersin[2][NMUL][400];
 	Float_t run[400];
 
 	for (int ir = 0; ir < Nrun; ir++)
@@ -41,7 +43,7 @@ void GetQAsincos()
 				{
 					int cent, harm, det;
 					Float_t sincos, sig;
-					if (id == NDET-1 && ih != 2) {f>>run[ir]>>cent>>harm>>det>>cos[ih][ic][ir]>>sig>>sin[ih][ic][ir]>>sig;}// for rxn18
+					if (id == 7 && ih != 2) {f>>run[ir]>>cent>>harm>>det>>cos[ih][ic][ir]>>ercos[ih][ic][ir]>>sin[ih][ic][ir]>>ersin[ih][ic][ir];}// for rxn18
 					else {f>>run[ir]>>cent>>harm>>det>>sincos>>sig>>sincos>>sig;}
 					if (cent != ic || harm != ih) {cout<<"incorrect "<<datafile[ir]<<" file data reading!! ic = "<<ic<<" cent = "<<cent<<" ih = "<<ih<<" harm = "<<harm<<endl; return;}
 				}
@@ -51,29 +53,22 @@ void GetQAsincos()
 	}
 	// ****
 
-	// make graph of t0 vs runnum for all the centralities and write them to the root file
-	TGraph *gr[2][NMUL];
+	// make graph of mean sin ans cos vs runnum for all the centralities for RXN18 det and write them to the root file
+	TGraph *gr[2][2][NMUL];
+	char name[20];
 
-	TFile *outfile = new TFile("MeanCos.root","recreate");
+	TFile *outfile = new TFile("MeanSinCosRXN17.root","recreate");
 				
 	for (int ic = 0; ic < NMUL; ic++)
 	{
 		for (int ih = 0; ih < 2; ih++ )
 		{
-			gr[ih][ic] = new TGraphErrors(Nrun, run, cos[ih][ic], 0, 0); 
-			gr[ih][ic]->Write();
-		}
-	}
-	outfile->Close();
-
-	TFile *outfile = new TFile("MeanSin.root","recreate");
-				
-	for (int ic = 0; ic < NMUL; ic++)
-	{
-		for (int ih = 0; ih < 2; ih++ )
-		{
-			gr[ih][ic] = new TGraphErrors(Nrun, run, sin[ih][ic], 0, 0); 
-			gr[ih][ic]->Write();
+			sprintf(name, "GrCos_h%i_c%i", ih, ic);
+			gr[0][ih][ic] = new TGraphErrors(Nrun, run, cos[ih][ic], 0, ercos[ih][ic]); 
+			gr[0][ih][ic]->Write(name);
+			sprintf(name, "GrSin_h%i_c%i", ih, ic);
+			gr[1][ih][ic] = new TGraphErrors(Nrun, run, sin[ih][ic], 0, ersin[ih][ic]);
+			gr[1][ih][ic]->Write(name);
 		}
 	}
 	outfile->Close();
